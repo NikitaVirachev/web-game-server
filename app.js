ec4644ca4ad5dea4e04385648ba87b471b3b8ae8
NikitaVirachev <guardsolder@gmail.com>
2025-02-03 23:37:28 +0300

    feat: add files for static route

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const morgan = require('morgan');
 const authRouter = require('./routes/authRoutes');
 
@@ -13,7 +14,16 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-// Routes
+// 2) Static files
+const frontendDir = process.env.FRONTEND_DIR;
+app.use(express.static(frontendDir));
+
+// Main route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(frontendDir, 'index.html'));
+});
+
+// 3) Routes
 app.use('/api/v1/login', authRouter);
 
 module.exports = app;

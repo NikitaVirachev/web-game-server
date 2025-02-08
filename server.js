726f033a4d7b8db5aa0bf86a3074c5ff9483cd9a
NikitaVirachev <guardsolder@gmail.com>
2025-02-08 13:43:05 +0300

    feat: connect to local DB

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+mongoose
+  .connect(process.env.DATABASE_LOCAL)
+  .then(() => console.log('DB connection successful!'))
+  .catch((err) => console.error('DB connection error:', err));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {

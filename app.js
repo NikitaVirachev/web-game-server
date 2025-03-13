25e506d4075127e0432e7e673c52f6c2cf312811
virachev.n <virachev.n@energosoft.net>
2025-03-13 14:20:32 +0300

    feat: handle unhandled routes

--- app.js
+++ app.js
@@ -26,4 +26,11 @@ app.get('/', (req, res) => {
 // 3) Routes
 app.use('/api/v1/users', userRouter);
 
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
+
 module.exports = app;

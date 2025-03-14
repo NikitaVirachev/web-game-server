ad8df208642ef51550c1f78636f3970f538036f1
NikitaVirachev <guardsolder@gmail.com>
2025-03-14 16:12:35 +0300

    feat: handle invalid databse _id

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,10 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -33,7 +40,11 @@ module.exports = (err, req, res, next) => {
 
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
-  } else {
-    sendErrorProd(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+
+    if (err.name === 'CastError') error = handleCastErrorDB(err);
+
+    sendErrorProd(error, res);
   }
 };

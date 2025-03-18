49c5d45ec0a15a8ac765adebd7fd5c2609267fb8
NikitaVirachev <guardsolder@gmail.com>
2025-03-18 16:46:10 +0300

    feat: return token in signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
@@ -23,8 +24,13 @@ exports.signup = catchAsync(async (req, res, next) => {
   const { email, password, passwordConfirm, name } = req.body;
   const newUser = await User.create({ email, password, passwordConfirm, name });
 
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
